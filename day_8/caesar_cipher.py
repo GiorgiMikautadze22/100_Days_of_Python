c44134b1de9da0104e8b886067160d7fc033ea57
Giorgi Mikautadze <giorgi.mikautadze2223@gmail.com>
2024-09-29 16:41:31 +0400

    feat: simplified caesar cipher logic

--- day_8/caesar_cipher.py
+++ day_8/caesar_cipher.py
@@ -7,27 +7,40 @@ def caesar_cipher():
     user_decode_input = input(f'What do you want to {decode_or_encode}? \n')
     user_count_input = int(input(f'With how many sets do you want to {decode_or_encode}? \n')) % len(alphabet)
 
-    user_input_split = list(user_decode_input)
+    # user_input_split = list(user_decode_input)
+    #
+    # new_list = []
+    # index_list = []
+    #
+    # result = ''
+    #
+    # for item in range(0, len(alphabet)):
+    #     if decode_or_encode == 'decode':
+    #         new_list += alphabet[item - user_count_input]
+    #     else:
+    #         new_list += alphabet[item - (len(alphabet) - user_count_input)]
+    #
+    # for item in user_input_split:
+    #     index_list.append(alphabet.index(item))
+    #
+    # for item in index_list:
+    #     result += new_list[item]
+    #
+    # print(result)
 
-    new_list = []
-    index_list = []
 
     result = ''
 
-    for item in range(0, len(alphabet)):
-        if decode_or_encode == 'decode':
-            new_list += alphabet[item - user_count_input]
-        else:
-            new_list += alphabet[item - (len(alphabet) - user_count_input)]
+    if decode_or_encode == "decode":
+        user_count_input *= -1
 
-    for item in user_input_split:
-        index_list.append(alphabet.index(item))
+    for char in user_decode_input:
+        index = alphabet.index(char) + user_count_input
+        index %= len(alphabet)
+        result += alphabet[index]
 
-    for item in index_list:
-        result += new_list[item]
 
     print(result)
-
     stop_or_continue = input('Do you want to try again? Yes or No \n').lower()
     if stop_or_continue == 'yes':
         caesar_cipher()
