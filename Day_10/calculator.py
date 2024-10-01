bb9f9c60feff064058a1173305d496e94ff35f1b
Giorgi Mikautadze <giorgi.mikautadze2223@gmail.com>
2024-10-01 14:20:34 +0400

    calculator_project

--- Day_10/calculator.py
+++ Day_10/calculator.py
@@ -1,6 +1,4 @@
-def calculator():
-    n1 = input("")
-
+import os
 
 def add(n1, n2):
     return n1 + n2
@@ -13,3 +11,26 @@ def multipy(n1, n2):
 
 def divide(n1, n2):
     return n1 / n2
+
+operations = {
+    '+': add,
+    '-': subtract,
+    '*': multipy,
+    '/': divide
+}
+
+initial_number = int(input("Input First Number: "))
+def calculator(n1):
+    method = input("What method do you want to perform? +, -, * or / : ")
+    n2 = int(input("Input Next Number: "))
+
+    result = operations[method](n1, n2)
+    print(f"{n1} {method} {n2} = {result}")
+    continue_calc = input('Do you want to continue calculation on this number? ').lower()
+    if continue_calc == 'yes':
+        os.system("cls")
+        calculator(result)
+    else:
+        return "Good By"
+
+calculator(initial_number)
