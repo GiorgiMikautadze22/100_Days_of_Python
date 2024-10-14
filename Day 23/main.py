3eef9c8cac482f63d0cd0cbf82ae4613879ae239
Giorgi Mikautadze <giorgi.mikautadze2223@gmail.com>
2024-10-14 20:02:14 +0400

    feat: added turtle moving func and  init car class

--- Day 23/main.py
+++ Day 23/main.py
@@ -8,3 +8,28 @@
 #TODO: When cars reach the end of the road they should be randomly regenerated from the start of the screen
 #TODO: When car hits the turtle game over
 #TODO: After turtle crosses the road increase level and speed
+
+from turtle import Screen
+from turtle_cross import TurtleCross
+
+screen = Screen()
+screen.setup(width=800, height=500)
+screen.title('Cross Road Game')
+screen.tracer(0)
+
+tim = TurtleCross(screen)
+
+screen.listen()
+screen.onkeypress(tim.up, 'Up')
+screen.onkeypress(tim.down, 'Down')
+screen.onkeypress(tim.turn_left, 'Left')
+screen.onkeypress(tim.turn_right, 'Right')
+
+
+
+
+screen.update()
+
+
+
+screen.exitonclick()
