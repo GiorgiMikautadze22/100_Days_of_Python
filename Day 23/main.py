f26b954c3dfdaf4ecafa90f94d24ec5eed82c748
Giorgi Mikautadze <giorgi.mikautadze2223@gmail.com>
2024-10-14 15:41:58 +0400

    feat: created todo list for the project

---
+++ Day 23/main.py
@@ -0,0 +1,10 @@
+#TODO: create screen and disable tracer
+#TODO: Create turtle class that can move on key binds
+#TODO: After turtle crosses the road direct it back to starting point
+#TODO: Create Car class
+#TODO: Cars should randomly be generated outside of the screen ( certain amount )
+#TODO: Cars should be different colors
+#TODO: Cars should move from right to left at a certain speed
+#TODO: When cars reach the end of the road they should be randomly regenerated from the start of the screen
+#TODO: When car hits the turtle game over
+#TODO: After turtle crosses the road increase level and speed
