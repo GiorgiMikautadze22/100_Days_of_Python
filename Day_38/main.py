fef7829ae1480eb3c03afa577d60348a88b16749
Giorgi Mikautadze <giorgi.mikautadze2223@gmail.com>
2024-11-10 11:24:29 +0400

    feat: added token authentication

--- Day_38/main.py
+++ Day_38/main.py
@@ -25,6 +25,7 @@ body = {
     "age": AGE
 }
 
+# Nutritionx request
 response = requests.post(url=os.getenv("BASE_API_URL"), json=body, headers=headers)
 response.raise_for_status()
 data = response.json()
@@ -32,6 +33,10 @@ data = response.json()
 # Sheety request
 today = datetime.now().strftime("%d/%m/%Y")
 time = datetime.now().strftime("%X")
+
+sheety_heaeder = {
+    "Authorization": "Bearer " + os.getenv("SHEETY_TOKEN")
+}
 
 for exercise in data["exercises"]:
     sheety_body = {
@@ -43,9 +48,5 @@ for exercise in data["exercises"]:
             "calories": exercise['nf_calories'],
         }
     }
-
-    print(sheety_body)
-
-    sheety_response = requests.post(url=os.getenv("SHEETY_API_URL"), json=sheety_body)
-    print(sheety_response.text)
+    sheety_response = requests.post(url=os.getenv("SHEETY_API_URL"), json=sheety_body, headers=sheety_heaeder)
     sheety_response.raise_for_status()
