0755562a208f6c07c776622f97fc0a72ac0484a0
Giorgi Mikautadze <giorgi.mikautadze2223@gmail.com>
2024-11-22 14:22:09 +0400

    feat: added static folder and html file
    This is day 56 contents

--- Day_54/hello.py
+++ Day_54/hello.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -9,7 +9,7 @@ def make_bold_decorator(function):
 @app.route("/")
 @make_bold_decorator
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
