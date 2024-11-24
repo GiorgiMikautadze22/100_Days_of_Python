da3eb708028b4eed1ec003a3191c945d784e7abd
Giorgi Mikautadze <giorgi.mikautadze2223@gmail.com>
2024-11-24 15:10:01 +0400

    added to blog page email sender

--- Day_59/main.py
+++ Day_59/main.py
@@ -1,9 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import smtplib
 import requests
 app = Flask(__name__)
 
 response = requests.get(url="https://api.npoint.io/674f5423f73deab1e9a7")
 data = response.json()
+
+def send_email(name, email, message):
+    email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nMessage:{message}"
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(OWN_EMAIL, OWN_PASSWORD)
+        connection.sendmail(OWN_EMAIL, OWN_EMAIL, email_message)
 
 @app.route('/')
 def get_all_posts():
@@ -13,9 +21,13 @@ def get_all_posts():
 def about():
     return render_template("about.html")
 
-@app.route('/contact')
+@app.route('/contact', methods=["POST", "GET"])
 def contact():
-    return render_template("contact.html")
+    if request.method == 'POST':
+        send_email(request.form["username"], request.form["email"], request.form["message"])
+        return render_template("contact.html", msg_sent=True)
+
+    return render_template("contact.html", msg_sent=False)
 
 @app.route("/blog/<int:index>")
 def show_post(index):
