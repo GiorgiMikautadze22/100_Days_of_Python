9157b89d844ef443a29325066c223dc8569fb0cf
Giorgi Mikautadze <giorgi.mikautadze2223@gmail.com>
2024-12-11 13:32:32 +0400

    refactor: changed imports placments

--- Day_68/main.py
+++ Day_68/main.py
@@ -1,14 +1,14 @@
 from flask import Flask, render_template, request, url_for, redirect, flash, send_from_directory
-from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String
-from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, EmailField
 from wtforms.validators import DataRequired, Email, Length, email
 from flask_bootstrap import Bootstrap5
 from werkzeug import security
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
 
 app = Flask(__name__)
 Bootstrap5(app)
